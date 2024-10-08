3653656fcac0400e13adca5e23557db9bbae676f
jockeygastin <jockeygastin1998@gmail.com>
2024-10-08 13:00:37 +0530

    ts ignored

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,10 +3,10 @@ import Faq from "@/components/Faq";
 import FeaturedProperties from "@/components/FeaturedProperties";
 import Hero from "@/components/Hero";
 import HowItWorks from "@/components/HowItWorks";
-import Navbar from "@/components/Navbar";
+// import Navbar from "@/components/Navbar";
 import Services from "@/components/Services";
 import Testimonials from "@/components/Testimonials";
-import Footer  from "@/components/Footer";
+// import Footer  from "@/components/Footer";
 
 const  Home = () => {
   return (
